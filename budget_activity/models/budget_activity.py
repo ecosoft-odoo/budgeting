1b62a3804db750cd80cc0e7b27da4b0b28934d94
ps-tubtim <pimolnats@ecosoft.co.th>
2021-03-17 18:09:39 +0700

    [IMP] budget_activity

--- budget_activity/models/budget_activity.py
+++ budget_activity/models/budget_activity.py
@@ -3,6 +3,23 @@
 from odoo import fields, models
 
 
+class BudgetActivityTag(models.Model):
+    _name = "budget.activity.tag"
+    _description = "Budget Activity Tag"
+
+    name = fields.Char(string="Name", required=True)
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env.company,
+        required=True,
+    )
+    active = fields.Boolean(
+        default=True,
+        help="Set active to false to hide the Budget Activity Tag without removing it.",
+    )
+
+
 class BudgetActivity(models.Model):
     _name = "budget.activity"
     _description = "Activity"
@@ -24,3 +41,11 @@ class BudgetActivity(models.Model):
         required=True,
     )
     code = fields.Char(related="account_id.code")
+    tag_ids = fields.Many2many(
+        comodel_name="budget.activity.tag",
+        relation="budget_activity_tag_rel",
+        column1="budget_activity_id",
+        column2="budget_activity_tag_id",
+        string="Tags",
+        help="Optional tags you may want to assign for search",
+    )
