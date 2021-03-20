e79e919458ee1bc3e9a559eb41488458f04fa325
Kitti U <kittiu@gmail.com>
2021-03-20 10:34:20 +0700

    Fine tune date_commit computation

--- budget_control/models/base_budget_move.py
+++ budget_control/models/base_budget_move.py
@@ -66,55 +66,65 @@ class BudgetDoclineMixin(models.AbstractModel):
 
     amount_commit = fields.Float(
         compute="_compute_commit",
+        copy=False,
         store=True,
     )
     date_commit = fields.Date(
         compute="_compute_commit",
         store=True,
-        readonly=False,
+        copy=False,
+        readonly=False,  # Allow manual entry of this field
     )
 
     @api.depends("budget_move_ids", "budget_move_ids.date")
     def _compute_commit(self):
+        """
+        - Calc amount_commit from all budget_move_ids
+        - Calc date_commit if not exists and on 1st budget_move_ids only or False
+        """
         for rec in self:
-            if not rec.budget_move_ids:
-                continue
             debit = sum(rec.budget_move_ids.mapped("debit"))
             credit = sum(rec.budget_move_ids.mapped("credit"))
             rec.amount_commit = debit - credit
-            rec.date_commit = min(rec.budget_move_ids.mapped("date"))
-
-    def _compute_date_commit(self):
-        """Default implementation, use date from _doc_date_field
-        which is mostly write_date during budget commitment"""
-        if not self._doc_date_fields:
-            raise ValidationError(_("_doc_date_fields is not set!"))
-        for rec in self:
-            if not rec[self._analytic_field]:
-                rec.date_commit = False
-                continue
-            dates = [
-                rec.mapped(f)[0]
-                for f in self._doc_date_fields
-                if rec.mapped(f)[0]
-            ]
-            if dates:
-                if isinstance(dates[0], datetime):
-                    rec.date_commit = fields.Datetime.context_timestamp(
-                        self, dates[0]
-                    )
-                else:
-                    rec.date_commit = dates[0]
+            if rec.budget_move_ids:
+                rec.date_commit = min(rec.budget_move_ids.mapped("date"))
             else:
                 rec.date_commit = False
-            # If the date_commit is not in analytic date range, use possible date.
-            # Note: another option is to have use choose manually.
-            date_from = rec[self._analytic_field].bm_date_from
-            date_to = rec[self._analytic_field].bm_date_to
-            if date_from and date_from > rec.date_commit:
-                rec.date_commit = date_from
-            elif date_to and date_to < rec.date_commit:
-                rec.date_commit = date_to
+
+    def _set_date_commit(self):
+        """Default implementation, use date from _doc_date_field
+        which is mostly write_date during budget commitment"""
+        self.ensure_one()
+        docline = self
+        if docline.date_commit:
+            return
+        if not self._doc_date_fields:
+            raise ValidationError(_("'_doc_date_fields' is not set!"))
+        if not docline[self._analytic_field]:
+            docline.date_commit = False
+            return
+        dates = [
+            docline.mapped(f)[0]
+            for f in self._doc_date_fields
+            if docline.mapped(f)[0]
+        ]
+        if dates:
+            if isinstance(dates[0], datetime):
+                docline.date_commit = fields.Datetime.context_timestamp(
+                    self, dates[0]
+                )
+            else:
+                docline.date_commit = dates[0]
+        else:
+            docline.date_commit = False
+        # If the date_commit is not in analytic date range, use possible date.
+        # Note: another option is to have use choose manually.
+        date_from = docline[self._analytic_field].bm_date_from
+        date_to = docline[self._analytic_field].bm_date_to
+        if date_from and date_from > docline.date_commit:
+            docline.date_commit = date_from
+        elif date_to and date_to < docline.date_commit:
+            docline.date_commit = date_to
 
     def _prepare_budget_commitment(
         self,
@@ -164,26 +174,25 @@ class BudgetDoclineMixin(models.AbstractModel):
         self.ensure_one()
         dom = [(f, "!=", False) for f in self._required_fields_to_commit()]
         docline = self.filtered_domain(dom)
-        docline._compute_date_commit()
+        docline._set_date_commit()
         docline._check_date_commit()  # Testing only, can be removed when stable.
         return docline and True or False
 
     def _check_date_commit(self):
         """ Commit date must inline with analytic account """
-        for rec in self:
-            if rec[self._analytic_field]:
-                if not rec.date_commit:
-                    raise UserError(_("No budget commitment date"))
-                date_from = rec[self._analytic_field].bm_date_from
-                date_to = rec[self._analytic_field].bm_date_to
-                if (date_from and date_from > rec.date_commit) or (
-                    date_to and date_to < rec.date_commit
-                ):
-                    raise UserError(
-                        _(
-                            "Budget commitment date is not in analytic date range"
-                        )
-                    )
-            else:
-                if rec.date_commit:
-                    raise UserError(_("Budget commitment date not required"))
+        self.ensure_one()
+        docline = self
+        if docline[self._analytic_field]:
+            if not docline.date_commit:
+                raise UserError(_("No budget commitment date"))
+            date_from = docline[self._analytic_field].bm_date_from
+            date_to = docline[self._analytic_field].bm_date_to
+            if (date_from and date_from > docline.date_commit) or (
+                date_to and date_to < docline.date_commit
+            ):
+                raise UserError(
+                    _("Budget commitment date is not in analytic date range")
+                )
+        else:
+            if docline.date_commit:
+                raise UserError(_("Budget commitment date not required"))
