87099d54b9430895767e898b026d3e764a075b74
Saran440 <saranl@ecosoft.co.th>
2021-06-05 17:39:37 +0700

    [FIX] transfer_dimension with domain

--- budget_control_transfer_dimension/models/budget_transfer_item.py
+++ budget_control_transfer_dimension/models/budget_transfer_item.py
@@ -7,6 +7,8 @@ from odoo.exceptions import UserError
 
 class BudgetTransferItem(models.Model):
     _inherit = "budget.transfer.item"
+    _analytic_tag_source_field_name = "source_analytic_tag_ids"
+    _analytic_tag_target_field_name = "target_analytic_tag_ids"
 
     source_analytic_tag_ids = fields.Many2many(
         comodel_name="account.analytic.tag",
@@ -14,6 +16,11 @@ class BudgetTransferItem(models.Model):
         relation="source_budget_control_analytic_tag_rel",
         column1="source_budget_control_id",
         column2="source_analytic_tag_id",
+    )
+    source_domain_tag_ids = fields.Many2many(
+        comodel_name="account.analytic.tag",
+        compute="_compute_source_analytic_tags_domain",
+        help="Helper field, the filtered tags_ids when record is saved",
     )
     source_analytic_tag_all = fields.Many2many(
         comodel_name="account.analytic.tag",
@@ -32,6 +39,103 @@ class BudgetTransferItem(models.Model):
         compute="_compute_target_analytic_tag_all",
         compute_sudo=True,
     )
+    target_domain_tag_ids = fields.Many2many(
+        comodel_name="account.analytic.tag",
+        compute="_compute_target_analytic_tags_domain",
+        help="Helper field, the filtered tags_ids when record is saved",
+    )
+
+    @api.depends(
+        lambda self: (self._analytic_tag_source_field_name,)
+        if self._analytic_tag_source_field_name
+        else ()
+    )
+    def _compute_source_analytic_tags_domain(self):
+        analytic_tag_field_name = self._analytic_tag_source_field_name
+        res = {}
+        for rec in self:
+            tag_ids = []
+            res = rec._dynamic_domain_transfer_analytic_tags(
+                analytic_tag_field_name
+            )
+            if res["domain"][analytic_tag_field_name]:
+                tag_ids = res["domain"][analytic_tag_field_name][0][2]
+            rec.source_domain_tag_ids = tag_ids
+
+    @api.depends(
+        lambda self: (self._analytic_tag_target_field_name,)
+        if self._analytic_tag_target_field_name
+        else ()
+    )
+    def _compute_target_analytic_tags_domain(self):
+        analytic_tag_field_name = self._analytic_tag_target_field_name
+        res = {}
+        for rec in self:
+            tag_ids = []
+            res = rec._dynamic_domain_transfer_analytic_tags(
+                analytic_tag_field_name
+            )
+            if res["domain"][analytic_tag_field_name]:
+                tag_ids = res["domain"][analytic_tag_field_name][0][2]
+            rec.target_domain_tag_ids = tag_ids
+
+    def _dynamic_domain_transfer_analytic_tags(self, analytic_tag_field_name):
+        """
+        - For dimension without by_sequence, always show
+        - For dimension with by_sequence, only show tags by sequence
+        - Option to filter next dimension based on selected_tags
+        """
+        Dimension = self.env["account.analytic.dimension"]
+        Tag = self.env["account.analytic.tag"]
+        # If no dimension with by_sequence, nothing to filter, exist
+        count = Dimension.search_count([("by_sequence", "=", True)])
+        if count == 0:
+            return {"domain": {analytic_tag_field_name: []}}
+        # Find non by_sequence tags, to show always
+        tags = Tag.search(
+            [
+                "|",
+                ("analytic_dimension_id", "=", False),
+                ("analytic_dimension_id.by_sequence", "=", False),
+            ]
+        )
+        # Find next dimension by_sequence
+        selected_tags = self[analytic_tag_field_name]
+        sequences = (
+            selected_tags.mapped("analytic_dimension_id")
+            .filtered("by_sequence")
+            .mapped("sequence")
+        )
+        cur_sequence = sequences and max(sequences) or -1
+        next_dimension = Dimension.search(
+            [("by_sequence", "=", True), ("sequence", ">", cur_sequence)],
+            order="sequence",
+            limit=1,
+        )
+        next_tag_ids = []
+        if next_dimension and next_dimension.filtered_field_ids:
+            # Filetered by previously selected_tags
+            next_tag_list = []
+            for field in next_dimension.filtered_field_ids:
+                matched_tags = selected_tags.filtered(
+                    lambda l: l.resource_ref
+                    and l.resource_ref._name == field.relation
+                )
+                tag_resources = matched_tags.mapped("resource_ref")
+                res_ids = tag_resources and [x.id for x in tag_resources] or []
+                tag_ids = next_dimension.analytic_tag_ids.filtered(
+                    lambda l: l.resource_ref[field.name].id in res_ids
+                ).ids
+                next_tag_list.append(set(tag_ids))
+            # "&" to all in next_tag_list
+            next_tag_ids = list(set.intersection(*map(set, next_tag_list)))
+        else:
+            next_tag_ids = next_dimension.analytic_tag_ids.ids
+        # Tags from non by_sequence dimension and next dimension
+        tag_ids = tags.ids + next_tag_ids
+        # tag_ids = tags.ids + next_tag_ids
+        domain = [("id", "in", tag_ids)]
+        return {"domain": {analytic_tag_field_name: domain}}
 
     def _get_dimension_fields(self):
         if self.env.context.get("update_custom_fields"):
@@ -55,7 +159,9 @@ class BudgetTransferItem(models.Model):
                 )
             ):
                 continue
-            doc.source_analytic_tag_ids = analytic_tag_ids
+            doc.source_analytic_tag_ids = (
+                len(analytic_tag_ids) == 1 and analytic_tag_ids or False
+            )
 
     @api.depends("target_budget_control_id")
     def _compute_target_analytic_tag_all(self):
@@ -72,25 +178,33 @@ class BudgetTransferItem(models.Model):
                 )
             ):
                 continue
-            doc.target_analytic_tag_ids = analytic_tag_ids
+            doc.target_analytic_tag_ids = (
+                len(analytic_tag_ids) == 1 and analytic_tag_ids or False
+            )
+
+    def _get_budget_allocation_line(self):
+        source_line = (
+            self.source_budget_control_id.allocation_line_ids.filtered(
+                lambda l: l.analytic_tag_ids == self.source_analytic_tag_ids
+            )
+        )
+        target_line = (
+            self.target_budget_control_id.allocation_line_ids.filtered(
+                lambda l: l.analytic_tag_ids == self.target_analytic_tag_ids
+            )
+        )
+        return source_line, target_line
+
+    def _check_constraint_transfer(self):
+        super()._check_constraint_transfer()
+        source_line, target_line = self._get_budget_allocation_line()
+        if not (source_line and target_line):
+            raise UserError(_("Invalid analytic tags!"))
 
     def transfer(self):
         res = super().transfer()
         for transfer in self:
-            source_line = (
-                transfer.source_budget_control_id.allocation_line_ids.filtered(
-                    lambda l: l.analytic_tag_ids
-                    == transfer.source_analytic_tag_ids
-                )
-            )
-            target_line = (
-                transfer.target_budget_control_id.allocation_line_ids.filtered(
-                    lambda l: l.analytic_tag_ids
-                    == transfer.target_analytic_tag_ids
-                )
-            )
-            if not (source_line and target_line):
-                raise UserError(_("Invalid analytic tags!"))
+            source_line, target_line = transfer._get_budget_allocation_line()
             source_line[0].budget_amount -= transfer.amount
             target_line[0].budget_amount += transfer.amount
         return res
