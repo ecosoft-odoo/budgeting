128f9cc56275fe0ddf96d5a8c2f801ad8beead9e
Saran440 <saranl@ecosoft.co.th>
2021-06-17 16:34:39 +0700

    [ADD] allocation_ou, access ou and source fund

--- budget_allocation_fund_department/__manifest__.py
+++ budget_allocation_fund_department/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Budget Allocation Fund - Revision Monitoring",
+    "name": "Budget Allocation Fund - Department Monitoring",
     "version": "14.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
@@ -10,7 +10,7 @@
     "website": "https://github.com/OCA/account-budgeting",
     "depends": [
         "budget_allocation_fund",
-        "budget_plan_revision",
+        "budget_control_department",
     ],
     "data": ["report/source_fund_monitor_report_view.xml"],
     "installable": True,
--- budget_allocation_fund_operating_unit_access_all/__manifest__.py
+++ budget_allocation_fund_operating_unit_access_all/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Revision Access OU Monitoring",
+    "name": "Allocation Fund - Access OU Monitoring",
     "version": "14.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "category": "Budget Management",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-budgeting",
     "depends": [
-        "budget_control_revision_operating_unit",
+        "budget_allocation_fund_operating_unit",
         "budget_control_operating_unit_access_all",
     ],
     "data": [],
