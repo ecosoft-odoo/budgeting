70d1214c1b10bf285a5d07c99f3903ec315185e7
Kitti U <kittiu@ecosoft.co.th>
2021-08-19 14:18:31 +0700

    Move some budget_control_xxx to account-budgeting

--- budget_control_purchase_request/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import purchase_request_budget_move
-from . import budget_period
-from . import purchase
-from . import purchase_request
-from . import budget_control
-from . import budget_move_forward
