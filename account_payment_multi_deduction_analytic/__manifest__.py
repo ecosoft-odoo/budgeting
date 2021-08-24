9404ff2b2c3ba5e0ded9cdfd8700bc2ea4e24513
ps-tubtim <pimolnats@ecosoft.co.th>
2021-08-24 10:56:27 +0700

    [ADD] account_payment_multi_deduction_analytic, account_payment_multi_deduction_activity

---
+++ account_payment_multi_deduction_analytic/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Analytic Account on Payment Register with Multiple Deduction",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-budgeting",
+    "category": "Accounting",
+    "depends": [
+        "account_payment_multi_deduction",
+        "analytic",
+    ],
+    "data": [
+        "wizard/account_payment_register_views.xml",
+    ],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["ps-tubtim"],
+}
---
+++ account_payment_multi_deduction_analytic/wizard/account_payment_register.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = "account.payment.register"
+
+    def _prepare_deduct_move_line(self, deduct):
+        vals = super()._prepare_deduct_move_line(deduct)
+        vals.update(
+            {
+                "analytic_account_id": deduct.analytic_account_id
+                and deduct.analytic_account_id.id
+                or False
+            }
+        )
+        return vals
