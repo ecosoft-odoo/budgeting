4de22312db198e8d4e279f6ea4ba708b183beca2
ps-tubtim <pimolnats@ecosoft.co.th>
2021-08-24 10:56:27 +0700

    [ADD] account_payment_multi_deduction_analytic, account_payment_multi_deduction_activity

---
+++ account_payment_multi_deduction_activity/wizard/account_payment_register.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = "account.payment.register"
+
+    def _prepare_deduct_move_line(self, deduct):
+        vals = super()._prepare_deduct_move_line(deduct)
+        if deduct.activity_id:
+            vals.update({"activity_id": deduct.activity_id.id})
+        return vals
