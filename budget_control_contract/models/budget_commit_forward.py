64d0805f1b1aaf519a38e7c1eabe89e20406f89a
Kitti U <kittiu@ecosoft.co.th>
2021-09-01 21:43:12 +0700

    Refactor carry forward

--- budget_control_contract/models/budget_commit_forward.py
+++ budget_control_contract/models/budget_commit_forward.py
@@ -20,35 +20,40 @@ class BudgetCommitForward(models.Model):
 
     def _get_budget_docline_model(self):
         res = super()._get_budget_docline_model()
-        if self.purchase:
+        if self.contract:
             res.append("contract.line")
         return res
 
     def _get_document_number(self, doc):
         if doc._name == "contract.line":
-            return ("{},{}".format(doc.sheet_id._name, doc.sheet_id.id),)
+            return "{},{}".format(doc.contract_id._name, doc.contract_id.id)
         return super()._get_document_number(doc)
 
-    def _get_domain_search(self, model):
-        domain_search = super()._get_domain_search(model)
-        if model == "contract.line":
-            domain_search.extend(
+    def _get_commit_docline(self, res_model):
+        if res_model == "contract.line":
+            domain = self._get_base_domain()
+            domain.extend(
                 [
                     ("analytic_account_id", "!=", False),
                     ("contract_id.commit_budget", "=", True),
                 ]
             )
-        return domain_search
+            return self.env[res_model].search(domain)
+        return super()._get_commit_docline(res_model)
 
 
 class BudgetCommitForwardLine(models.Model):
     _inherit = "budget.commit.forward.line"
 
     res_model = fields.Selection(
-        selection_add=[("contract.line", "Contract")],
+        selection_add=[("contract.line", "Contract Line")],
         ondelete={"contract.line": "cascade"},
     )
     document_id = fields.Reference(
-        selection_add=[("contract.line", "Contract")],
+        selection_add=[("contract.line", "Contract Line")],
         ondelete={"contract.line": "cascade"},
+    )
+    document_number = fields.Reference(
+        selection_add=[("contract.contract", "Contract")],
+        ondelete={"contract.contract": "cascade"},
     )
