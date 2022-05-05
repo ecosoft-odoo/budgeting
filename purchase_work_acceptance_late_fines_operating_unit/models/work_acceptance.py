e3fdf318514897eeaabf971ce6d235443d289eb3
ps-tubtim <pimolnats@ecosoft.co.th>
2022-05-05 15:39:37 +0700

    [FIX] purchase_work_acceptance_late_fines_operating_unit

--- purchase_work_acceptance_late_fines_operating_unit/models/work_acceptance.py
+++ purchase_work_acceptance_late_fines_operating_unit/models/work_acceptance.py
@@ -7,9 +7,21 @@ from odoo import models
 class WorkAcceptance(models.Model):
     _inherit = "work.acceptance"
 
+    def _prepare_late_wa_moves(self, move_type):
+        res = super()._prepare_late_wa_moves(move_type)
+        work_acceptance = self.env["work.acceptance"]
+        for rec in res:
+            wa = work_acceptance.browse(rec["late_wa_id"])
+            operating_unit_ids = wa.wa_line_ids.mapped(
+                "purchase_line_id.operating_unit_id"
+            )
+            if len(operating_unit_ids) == 1:
+                rec["operating_unit_id"] = operating_unit_ids.id
+        return res
+
     def _prepare_late_wa_move_line(self, name=False):
         move_line = super()._prepare_late_wa_move_line(name=name)
-        # Update fund_id
+        # Update operating_unit_id
         operating_unit_ids = self.wa_line_ids.mapped(
             "purchase_line_id.operating_unit_id"
         )
