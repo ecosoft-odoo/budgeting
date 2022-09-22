41204d0411ac4f54f73351fc5e0b537fd630bd2e
Saran440 <saranl@ecosoft.co.th>
2022-09-22 10:44:32 +0700

    [FIX] carry forward contract

--- budget_control_contract/models/__init__.py
+++ budget_control_contract/models/__init__.py
@@ -4,6 +4,7 @@ from . import base_budget_move
 from . import contract_budget_move
 from . import budget_period
 from . import contract
+from . import contract_line
 from . import account_move
 from . import account_move_line
 from . import budget_control
