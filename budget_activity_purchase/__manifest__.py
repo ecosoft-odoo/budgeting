01845f277f4ad0595358032dc7dbf4f0a1d3a040
Saran440 <saranl@ecosoft.co.th>
2023-01-11 10:55:58 +0700

    [ADD] budgeting

---
+++ budget_activity_purchase/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Budget Activity - Purchase",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "budget_activity",
+        "budget_control_purchase",
+    ],
+    "data": [
+        "views/purchase_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "maintainers": ["kittiu"],
+    "development_status": "Alpha",
+}
