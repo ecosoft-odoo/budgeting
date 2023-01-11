dd9d1f20cb88ea77c84ebce7bc97420ad31e1113
Saran440 <saranl@ecosoft.co.th>
2023-01-11 10:55:58 +0700

    [ADD] budgeting

---
+++ budget_activity/models/budget_period.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class BudgetPeriod(models.Model):
+    _inherit = "budget.period"
+
+    @api.model
+    def _get_kpi_by_control_key(self, template_lines, control):
+        activity_id = control["activity_id"]
+        template_line = self._get_filter_template_line(template_lines, control)
+        if len(template_line) == 1:
+            return template_line
+        # Invalid Template Lines
+        activity = self.env["budget.activity"].browse(activity_id)
+        if not template_line:
+            raise UserError(
+                _("Chosen activity %s is not valid for budgeting")
+                % activity.display_name
+            )
+        raise UserError(
+            _(
+                "Template Lines has more than one KPI being "
+                "referenced by the same account code %s"
+            )
+            % (activity.display_name)
+        )
+
+    def _get_filter_template_line(self, all_template_lines, control):
+        """Overwrite filter template line from account_id to activity_id"""
+        activity_id = control["activity_id"]
+        template_lines = all_template_lines.filtered(
+            lambda l: activity_id in l.activity_ids.ids
+        )
+        return template_lines
---
+++ budget_activity/readme/USAGE.rst
@@ -0,0 +1,12 @@
+To used this module you have to configued Budget Activity first.
+
+#. Go to Budgeting > Activity > Budget Activity
+#. Create new activity, select a KPI (if you want to group it) and match the activity to an account.
+#. Add a `Keyword` if you need to search for other words to show this activity. For example, the activity name is `Activity1` and the keywords are `Ticket` and `Transportation`. In the Activity field, the user can search for the name `Ticket` to see the activity `Activity1`.
+#. Go to Budgeting > Configurations > Budget Template
+#. Create new template or use old template (if you have).
+#. When you select a KPI, it will automatically select the corresponding activity and account. If you haven't set up an activity in the KPI, you can select the activity and it will automatically select the corresponding account.
+
+
+In the usage window, you will see a new field called "Activity" where the user can select from a list of options.
+The system will then match the selected activity to the corresponding account that has been set up.
