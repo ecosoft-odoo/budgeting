03d5d2726eef0404f66b640c2eaca361524f9293
ps-tubtim <pimolnats@ecosoft.co.th>
2023-01-11 10:18:26 +0700

    Initial 15.0

---
+++ account_payment_multi_deduction_activity/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Payment Register Diff - Activity",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-budgeting",
+    "category": "Accounting",
+    "depends": [
+        "base_budget_account_payment_multi_deduction",
+        "budget_activity",
+    ],
+    "data": [
+        "wizard/account_payment_register_views.xml",
+    ],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["ps-tubtim"],
+}
---
+++ account_payment_multi_deduction_activity/wizard/account_payment_deduction.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, models
+
+
+class AccountPaymentDeduction(models.TransientModel):
+    _inherit = "account.payment.deduction"
+
+    @api.onchange("activity_id")
+    def _onchange_activity_id(self):
+        if self.activity_id:
+            self.account_id = self.activity_id.account_id
