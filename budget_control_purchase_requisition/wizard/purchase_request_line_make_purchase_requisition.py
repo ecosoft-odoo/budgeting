682b0180e49c9c4056cfc129a65adf97d0637f87
ps-tubtim <pimolnats@ecosoft.co.th>
2023-01-11 10:18:26 +0700

    Initial 15.0

---
+++ budget_control_purchase_requisition/wizard/purchase_request_line_make_purchase_requisition.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class PurchaseRequestLineMakePurchaseRequisition(models.TransientModel):
+    _inherit = "purchase.request.line.make.purchase.requisition"
+
+    @api.model
+    def _prepare_purchase_requisition_line(self, pr, item):
+        pr_line_dict = super()._prepare_purchase_requisition_line(pr, item)
+        if item.line_id.fwd_analytic_account_id:
+            pr_line_dict[
+                "account_analytic_id"
+            ] = item.line_id.fwd_analytic_account_id.id
+        return pr_line_dict
