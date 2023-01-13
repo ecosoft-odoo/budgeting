a8deb98b7ab9249d28450e1383159dc0e7943592
Saran440 <saranl@ecosoft.co.th>
2023-01-13 17:49:20 +0700

    [ADD] budget_allocation_purchase

---
+++ budget_allocation_purchase/models/purchase.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PurchaseOrderLine(models.Model):
+    _name = "purchase.order.line"
+    _inherit = ["analytic.dimension.line", "purchase.order.line"]
+    _budget_analytic_field = "account_analytic_id"
+    _analytic_tag_field_name = "analytic_tag_ids"
+
+    def _prepare_account_move_line(self, move=False):
+        res = super()._prepare_account_move_line(move)
+        res["fund_id"] = self.fund_id.id
+        return res
