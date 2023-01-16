ad3cc04bb5ad73c53c9803004a8bccd12e72886f
Saran440 <saranl@ecosoft.co.th>
2023-01-16 11:09:34 +0700

    [ADD] budget_allocation_purchase_request

--- budget_allocation_advance_clearing/tests/test_budget_allocation_advance.py
+++ budget_allocation_advance_clearing/tests/test_budget_allocation_advance.py
@@ -50,7 +50,7 @@ class TestBudgetAllocationAdvance(TestBudgetAllocation):
         return expense_sheet
 
     @freeze_time("2001-02-01")
-    def test_01_commitment_expense_fund(self):
+    def test_01_commitment_advance_fund(self):
         """Create same analytic, difference fund, difference analytic tags
         line 1: Costcenter1, Fund1, Tag1, 50.0
         line 2: Costcenter1, Fund1, Tag2, 100.0
