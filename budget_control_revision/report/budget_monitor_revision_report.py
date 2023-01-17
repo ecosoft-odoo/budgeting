8b9ebd1d9fe7522cfeced60a2a04b8b8ac5c18d8
Saran440 <saranl@ecosoft.co.th>
2023-01-17 12:17:20 +0700

    [ADD] budget_plan_revision

--- budget_control_revision/report/budget_monitor_revision_report.py
+++ budget_control_revision/report/budget_monitor_revision_report.py
@@ -16,3 +16,15 @@ class BudgetMonitorRevisionReport(models.Model):
         """To see the previous version, active can be false."""
         sql_from = super()._from_budget()
         return sql_from.replace("and b.active = true", "")
+
+    def _get_sql(self):
+        """Not query commitment in revision monitoring"""
+        select_budget_query = self._select_budget()
+        key_select_budget_list = sorted(select_budget_query.keys())
+        select_budget = ", ".join(
+            select_budget_query[x] for x in key_select_budget_list
+        )
+        return "(select {} {})".format(
+            select_budget,
+            self._from_budget(),
+        )
