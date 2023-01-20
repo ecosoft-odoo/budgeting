ed9e7eaa1c6e545bec0396a5ebbb844b5738cae7
Saran440 <saranl@ecosoft.co.th>
2023-01-20 12:01:54 +0700

    [ADD] budget_activity_purchase_requisition

--- budget_control_purchase_requisition/__manifest__.py
+++ budget_control_purchase_requisition/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Budget Control - Purchase Agreement",
+    "name": "Budget Control - Purchase Requisition",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
