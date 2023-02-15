d38955e67a50bad7a2d3fc2ef9e744e79ec1e9a6
Saran440 <saranl@ecosoft.co.th>
2023-02-15 13:54:02 +0700

    [FIX] Not check budget available with kpi advance

---
+++ budget_activity_advance_clearing/models/budget_period.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class BudgetPeriod(models.Model):
+    _inherit = "budget.period"
+
+    def _get_budget_avaiable(self, analytic_id, template_lines):
+        """Not check budget available with kpi advance"""
+        kpi_advance = self.env.ref(
+            "budget_activity_advance_clearing.budget_kpi_advance"
+        )
+        template_lines = template_lines.filtered(lambda l: l.kpi_id != kpi_advance)
+        return super()._get_budget_avaiable(analytic_id, template_lines)
