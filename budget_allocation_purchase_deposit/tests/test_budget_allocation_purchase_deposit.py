a8c783b3fe7b73f805210a16a716a36c6c92fa1b
Saran440 <saranl@ecosoft.co.th>
2023-02-22 18:06:20 +0700

    [15.0][ADD] budget_allocation_purchase_deposit

---
+++ budget_allocation_purchase_deposit/tests/test_budget_allocation_purchase_deposit.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Ecosoft Co., Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo.addons.purchase_deposit_analytic.tests.test_purchase_deposit_analytic import (
+    TestPurchaseDepositAnalytic,
+)
+
+
+class TestPurchaseDepositAnalytic(TestPurchaseDepositAnalytic):
+    def setUp(self):
+        super().setUp()
