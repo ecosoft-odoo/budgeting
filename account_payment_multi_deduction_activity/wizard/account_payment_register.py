468d28c6c2a0b8853c97b8902c3b1d757b7b5f75
Saran440 <saranl@ecosoft.co.th>
2023-02-28 17:47:44 +0700

    [ADD] account_payment_multi_deudction_budget_allocation, account_payment_multi_deduction_activity

---
+++ account_payment_multi_deduction_activity/wizard/account_payment_register.py
@@ -0,0 +1,30 @@
+# Copyright 2023 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _name = "account.payment.register"
+    _inherit = ["account.payment.register", "budget.docline.mixin.base"]
+
+    @api.onchange("activity_id")
+    def _onchange_activity_id(self):
+        if self.activity_id:
+            self.writeoff_account_id = self.activity_id.account_id
+
+    def _prepare_deduct_move_line(self, deduct):
+        vals = super()._prepare_deduct_move_line(deduct)
+        vals.update(
+            {"activity_id": deduct.activity_id and deduct.activity_id.id or False}
+        )
+        return vals
+
+    def _create_payment_vals_from_wizard(self):
+        payment_vals = super()._create_payment_vals_from_wizard()
+        if (
+            not self.currency_id.is_zero(self.payment_difference)
+            and self.payment_difference_handling == "reconcile"
+        ):
+            payment_vals["write_off_line_vals"]["activity_id"] = self.activity_id.id
+        return payment_vals
