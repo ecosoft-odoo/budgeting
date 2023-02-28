abf683f19f30c9cc2b5e1fe358129d4908e6408f
Saran440 <saranl@ecosoft.co.th>
2023-02-28 13:44:34 +0700

    [FIX] transfer tier validation

---
+++ budget_control_tier_validation/models/budget_transfer.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class BudgetTransfer(models.Model):
+    _name = "budget.transfer"
+    _inherit = ["budget.transfer", "tier.validation"]
+    _state_from = ["submit"]
+    _state_to = ["transfer"]
+    _tier_validation_manual_config = False
