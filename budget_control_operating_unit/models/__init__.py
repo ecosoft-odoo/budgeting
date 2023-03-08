a326ac24d3812a784752e31021759fab9404344f
Tharathip <tharathipc@ecosoft.co.th>
2023-03-08 11:49:52 +0700

    [ENH] Filter operating unit in actual budget move report

--- budget_control_operating_unit/models/__init__.py
+++ budget_control_operating_unit/models/__init__.py
@@ -5,6 +5,7 @@ from . import res_config_settings
 from . import operating_unit
 from . import base_budget_move
 from . import account_analytic_account
+from . import account_budget_move
 from . import budget_control
 from . import budget_transfer_item
 from . import budget_transfer
