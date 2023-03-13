bdf72aa87472aeea1d3720b35097fddcf8588358
Saran440 <saranl@ecosoft.co.th>
2023-03-13 11:30:48 +0700

    [ENH] control budget with details and rename module

--- l10n_th_gov_purchase_agreement_budget_control/models/budget_period.py
+++ l10n_th_gov_purchase_agreement_budget_control/models/budget_period.py
@@ -1,18 +1,11 @@
 # Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import models
 
 
 class BudgetPeriod(models.Model):
     _inherit = "budget.period"
-
-    agreement = fields.Boolean(
-        string="On Agreement",
-        default=True,
-        readonly=True,
-        help="Control budget on purchase (agreement) approved",
-    )
 
     def _budget_info_query(self):
         query = super()._budget_info_query()
