40aa19f6788104b3d6974c2b379f5e6330ea4711
Saran440 <saranl@ecosoft.co.th>
2023-03-15 12:43:20 +0700

    [FIX] purchase_stock_budget_allocation - depend stock_budget_allocation

--- purchase_stock_budget_allocation/tests/test_purchase_stock_budget_allocation.py
+++ purchase_stock_budget_allocation/tests/test_purchase_stock_budget_allocation.py
@@ -1,7 +1,6 @@
 # Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 from odoo.addons.purchase_stock_analytic.tests.test_purchase_stock_analytic import (
     TestPurchaseStockAnalytic,
 )
