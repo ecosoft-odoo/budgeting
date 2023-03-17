bf70b9182043b70fcc2e80701e9224d936cb4c9a
Saran440 <saranl@ecosoft.co.th>
2023-03-17 13:40:12 +0700

    [15.0][ADD] budget_allocation_contract_invoice_plan

---
+++ budget_allocation_contract_invoice_plan/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Budget Allocation - Contract Invoice Plan",
+    "summary": "Allocate budget for contract invoice plan",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "budget_allocation_contract",
+        "contract_invoice_plan",
+    ],
+    "data": ["views/contract_view.xml"],
+    "installable": True,
+    "auto_install": True,
+    "maintainers": ["Saran440"],
+}
