d29022b14b8c53d8667b20ac51b083250b47dfe1
Saran440 <saranl@ecosoft.co.th>
2023-04-07 12:10:00 +0700

    [FIX] budget_allocation in register payment

--- account_payment_multi_deduction_budget_allocation/wizard/account_payment_deduction.py
+++ account_payment_multi_deduction_budget_allocation/wizard/account_payment_deduction.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class AccountPaymentDeduction(models.TransientModel):
@@ -9,11 +9,38 @@ class AccountPaymentDeduction(models.TransientModel):
     _inherit = [
         "analytic.dimension.line",
         "account.payment.deduction",
-        "budget.docline.mixin.base",
     ]
+
+    writeoff_fund_all = fields.Many2many(comodel_name="budget.source.fund")
+    writeoff_fund_id = fields.Many2one(
+        comodel_name="budget.source.fund",
+        string="Fund",
+        index=True,
+        tracking=True,
+        ondelete="restrict",
+        domain="[('id', 'in', writeoff_fund_all)]",
+    )
+    writeoff_analytic_tag_all = fields.Many2many(
+        comodel_name="account.analytic.tag",
+        relation="account_payment_deduction_tag_all_rel",
+        column1="register_id",
+        column2="tag_all_id",
+    )
+
+    @api.onchange("analytic_account_id")
+    def _onchange_budget_all(self):
+        for rec in self:
+            allocation_lines = rec.analytic_account_id.allocation_line_ids
+            rec.writeoff_fund_all = allocation_lines.mapped("fund_id")
+            rec.writeoff_fund_id = (
+                rec.writeoff_fund_all._origin.id
+                if len(rec.writeoff_fund_all) == 1
+                else False
+            )
+            rec.writeoff_analytic_tag_all = allocation_lines.mapped("analytic_tag_ids")
 
     @api.depends("payment_id")
     def _compute_analytic_multi_deduction(self):
         res = super()._compute_analytic_multi_deduction()
-        self._onchange_fund_all()
+        self._onchange_budget_all()
         return res
--- account_payment_multi_deduction_budget_allocation/wizard/account_payment_register.py
+++ account_payment_multi_deduction_budget_allocation/wizard/account_payment_register.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class AccountPaymentRegister(models.TransientModel):
@@ -9,14 +9,48 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = [
         "analytic.dimension.line",
         "account.payment.register",
-        "budget.docline.mixin.base",
     ]
-    _budget_analytic_field = "writeoff_analytic_account_id"
     _analytic_tag_field_name = "writeoff_analytic_tag_ids"
+
+    writeoff_fund_id = fields.Many2one(
+        comodel_name="budget.source.fund",
+        string="Fund",
+        index=True,
+        tracking=True,
+        ondelete="restrict",
+        domain="[('id', 'in', writeoff_fund_all)]",
+    )
+    writeoff_fund_all = fields.Many2many(
+        comodel_name="budget.source.fund",
+    )
+    writeoff_analytic_tag_all = fields.Many2many(
+        comodel_name="account.analytic.tag",
+        relation="account_payment_register_tag_all_rel",
+        column1="register_id",
+        column2="tag_all_id",
+    )
+
+    @api.onchange("writeoff_analytic_account_id")
+    def _onchange_budget_all(self):
+        for rec in self:
+            allocation_lines = rec.writeoff_analytic_account_id.allocation_line_ids
+            rec.writeoff_fund_all = allocation_lines.mapped("fund_id")
+            rec.writeoff_fund_id = (
+                rec.writeoff_fund_all._origin.id
+                if len(rec.writeoff_fund_all) == 1
+                else False
+            )
+            rec.writeoff_analytic_tag_all = allocation_lines.mapped("analytic_tag_ids")
 
     def _prepare_deduct_move_line(self, deduct):
         vals = super()._prepare_deduct_move_line(deduct)
-        vals.update({"fund_id": deduct.fund_id and deduct.fund_id.id or False})
+        vals.update(
+            {
+                "writeoff_fund_id": deduct.writeoff_fund_id
+                and deduct.writeoff_fund_id.id
+                or False
+            }
+        )
         return vals
 
     def _create_payment_vals_from_wizard(self):
@@ -25,10 +59,13 @@ class AccountPaymentRegister(models.TransientModel):
             not self.currency_id.is_zero(self.payment_difference)
             and self.payment_difference_handling == "reconcile"
         ):
-            payment_vals["write_off_line_vals"]["fund_id"] = self.fund_id.id
+            payment_vals["write_off_line_vals"]["fund_id"] = self.writeoff_fund_id.id
         return payment_vals
 
     def _update_vals_deduction(self, moves):
         res = super()._update_vals_deduction(moves)
-        self._onchange_fund_all()
+        move_lines = moves.mapped("line_ids")
+        self.writeoff_fund_all = move_lines.mapped("fund_all")
+        self.writeoff_analytic_tag_all = move_lines.mapped("analytic_tag_all")
+        self._onchange_budget_all()
         return res
