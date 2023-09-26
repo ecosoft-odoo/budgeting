ea23e91730803b295f7cb87dfa7ee5d9685637d4
Saran440 <saranl@ecosoft.co.th>
2023-09-26 13:58:08 +0700

    [ADD] budget_control_job

---
+++ budget_control_job/models/budget_balance_forward.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, fields, models
+
+
+class BudgetBalanceForward(models.Model):
+    _inherit = "budget.balance.forward"
+
+    job_uuid = fields.Char(
+        string="Job UUID",
+        readonly=True,
+    )
+
+    def action_budget_balance_forward_job(self):
+        description = _("Creating forward balance budget with id %s") % (self.id,)
+        job = self.with_delay(description=description).action_budget_balance_forward()
+        # Update UUID in forward commit
+        self.job_uuid = job.uuid
+        return "Job created with uuid {}".format(job.uuid)
---
+++ budget_control_job/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module add job queue for run carry forward with many records.
