8811072b04980d6c478b18355ac6dfb8aaaddb5f
Saran440 <saranl@ecosoft.co.th>
2024-09-24 15:20:02 +0700

    [IMP] budget_control: multi-company budget adjustment

--- budget_control/models/budget_control.py
+++ budget_control/models/budget_control.py
@@ -84,7 +84,6 @@ class BudgetControl(models.Model):
     )
     company_id = fields.Many2one(
         comodel_name="res.company",
-        string="Company",
         default=lambda self: self.env.company,
         required=True,
         readonly=True,
