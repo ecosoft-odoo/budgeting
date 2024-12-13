6e065915752ba7f2baf082f0bada0c63b7d7e8c3
Saran440 <saranl@ecosoft.co.th>
2024-12-13 17:03:42 +0700

    [FIX] budget_*: change sequence to 2 digits

--- budget_allocation_purchase_request/report/budget_source_fund_report.py
+++ budget_allocation_purchase_request/report/budget_source_fund_report.py
@@ -11,7 +11,7 @@ class SourceFundMonitorReport(models.Model):
         return super()._get_consumed_sources() + [
             {
                 "model": ("purchase.request.line", "Purchase Request Line"),
-                "type": ("2_pr_commit", "PR Commit"),
+                "type": ("20_pr_commit", "PR Commit"),
                 "budget_move": (
                     "purchase_request_budget_move",
                     "purchase_request_line_id",
@@ -21,10 +21,10 @@ class SourceFundMonitorReport(models.Model):
         ]
 
     def _get_sql(self):
-        select_pr_query = self._select_statement("2_pr_commit")
+        select_pr_query = self._select_statement("20_pr_commit")
         key_select_list = sorted(select_pr_query.keys())
         select_pr = ", ".join(select_pr_query[x] for x in key_select_list)
         return super()._get_sql() + "union (select {} {})".format(
             select_pr,
-            self._from_statement("2_pr_commit"),
+            self._from_statement("20_pr_commit"),
         )
