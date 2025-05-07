102e2beb3efef6d436f2c9a56dd97b2d090de0c3
Saran440 <saranl@ecosoft.co.th>
2025-05-07 14:01:16 +0700

    [IMP] budget_control_request_document_*: Change flow commit budget

--- budget_control_request_document_expense/models/__init__.py
+++ budget_control_request_document_expense/models/__init__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import request_budget_move
+from . import base_budget_move
 from . import request_document
+from . import request_order
 from . import hr_expense
-from . import budget_commit_forward
