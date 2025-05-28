a4c091449dac5d94c29f8a4b475fc6da375775f2
Saran440 <saranl@ecosoft.co.th>
2025-05-28 03:42:04 +0000

    [BOT] post-merge updates

--- budget_control_request_document_expense/__manifest__.py
+++ budget_control_request_document_expense/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Budget Control on Request Document Expense",
-    "version": "15.0.1.0.0",
+    "version": "15.0.2.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/ecosoft-odoo/budgeting",
