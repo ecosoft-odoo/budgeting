099c1b08c9f6bbaea39cdfda2b6f50ffb88ef6c3
Saran440 <saranl@ecosoft.co.th>
2025-06-16 06:44:55 +0000

    [BOT] post-merge updates

--- budget_control_revision/__manifest__.py
+++ budget_control_revision/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Budget Control - Revisions",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Keep track of revised budget control",
