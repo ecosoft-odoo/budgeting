cbe324db90d1867caf2a7899e8a175210884078c
NAKROB SAMAEL <nakrobs@ecosoft.co.th>
2025-06-19 15:21:37 +0700

    [IMP] budget_control_exception: pre-commit auto fixes

--- budget_control_exception/tests/test_budget_control_exception.py
+++ budget_control_exception/tests/test_budget_control_exception.py
@@ -1,57 +1,64 @@
 # Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import Command
 from odoo.tests import tagged
 from odoo.tests.common import Form
 
-from odoo.addons.budget_control.tests.common import BudgetControlCommon
+from odoo.addons.budget_control.tests.common import get_budget_common_class
 
 
 @tagged("post_install", "-at_install")
-class TestBudgetControlException(BudgetControlCommon):
+class TestBudgetControlExpense(get_budget_common_class()):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Create budget plan with 1 analytic
+        lines = [
+            Command.create(
+                {"analytic_account_id": cls.costcenter1.id, "amount": 2400.0}
+            )
+        ]
+        cls.budget_plan = cls.create_budget_plan(
+            cls,
+            name="Test - Plan {cls.budget_period.name}",
+            budget_period=cls.budget_period,
+            lines=lines,
+        )
+        cls.budget_plan.action_confirm()
+        cls.budget_plan.action_create_update_budget_control()
+        cls.budget_plan.action_done()
+        # Refresh data
+        cls.budget_plan.invalidate_recordset()
+
+        cls.budget_control = cls.budget_plan.budget_control_ids
+        cls.budget_control.template_line_ids = [
+            cls.template_line1.id,
+            cls.template_line2.id,
+            cls.template_line3.id,
+        ]
+        # Test item created for 3 kpi x 4 quarters = 12 budget items
+        cls.budget_control.prepare_budget_control_matrix()
+        assert len(cls.budget_control.line_ids) == 12
+        # Assign budget.control amount: KPI1 = 100x4=400, KPI2=800, KPI3=1,200
+        cls.budget_control.line_ids.filtered(lambda x: x.kpi_id == cls.kpi1).write(
+            {"amount": 100}
+        )
+        cls.budget_control.line_ids.filtered(lambda x: x.kpi_id == cls.kpi2).write(
+            {"amount": 200}
+        )
+        cls.budget_control.line_ids.filtered(lambda x: x.kpi_id == cls.kpi3).write(
+            {"amount": 300}
+        )
         cls.BudgetControlExceptionConfirm = cls.env["budget.control.exception.confirm"]
-        # Add group budget in partner
         cls.demo_user = cls.env.ref("base.user_demo")
         cls.partner_assign = cls.demo_user.partner_id
-        cls.env.ref("budget_control.group_budget_control_user").write(
-            {"users": [(4, cls.demo_user.id)]}
-        )
         cls.exception_checkassignee = cls.env.ref(
             "budget_control_exception.bc_excep_assignee_check"
         )
         cls.exception_checkamount = cls.env.ref(
             "budget_control_exception.bc_excep_amount_plan_check"
         )
-        # Create sample ready to use Budget Control
-        cls.budget_control = cls.BudgetControl.create(
-            {
-                "name": "CostCenter1/%s" % cls.year,
-                "template_id": cls.budget_period.template_id.id,
-                "budget_period_id": cls.budget_period.id,
-                "analytic_account_id": cls.costcenter1.id,
-                "plan_date_range_type_id": cls.date_range_type.id,
-                "template_line_ids": [
-                    cls.template_line1.id,
-                    cls.template_line2.id,
-                    cls.template_line3.id,
-                ],
-            }
-        )
-        # Test item created for 3 kpi x 4 quarters = 12 budget items
-        cls.budget_control.prepare_budget_control_matrix()
-        assert len(cls.budget_control.line_ids) == 12
-        # # Assign budget.control amount: KPI1 = 100, KPI2=800, Total=300
-        # cls.budget_control.line_ids.filtered(lambda x: x.kpi_id == cls.kpi1)[:1].write(
-        #     {"amount": 100}
-        # )
-        # cls.budget_control.line_ids.filtered(lambda x: x.kpi_id == cls.kpi2)[:1].write(
-        #     {"amount": 200}
-        # )
-        # cls.budget_control.flush()  # Need to flush data into table, so it can be sql
-        # cls.budget_control.allocated_amount = 300
 
     def _check_normal_process(self):
         self.assertEqual(self.budget_control.state, "draft")
@@ -96,6 +103,9 @@ class TestBudgetControlException(BudgetControlCommon):
         self._check_normal_process()
         # Exception Case
         self.budget_control.line_ids[0].amount = -1
+        total_amount = sum(self.budget_control.line_ids.mapped("amount"))
+        self.budget_control.released_amount = total_amount
+
         self.assertEqual(self.budget_control.state, "draft")
         self.budget_control.action_done()
         self.assertEqual(self.budget_control.state, "draft")
