c4c05c6cb2b1e764aad98f9117d443909d18c2af
Saran440 <saranl@ecosoft.co.th>
2026-06-27 02:02:27 +0000

    [BOT] post-merge updates

--- budget_control_expense/__manifest__.py
+++ budget_control_expense/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Budget Control on Expense",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.3.1",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/ecosoft-odoo/budgeting",
