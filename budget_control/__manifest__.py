5fd8f14e0c073285478d67e740d368b35c80cfc7
Saran440 <saranl@ecosoft.co.th>
2026-07-14 11:10:32 +0000

    [BOT] post-merge updates

--- budget_control/__manifest__.py
+++ budget_control/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Budget Control",
-    "version": "18.0.1.10.2",
+    "version": "18.0.1.11.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
