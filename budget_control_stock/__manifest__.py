4d83b4296c3fa4f7dc3fbf46f28c85e9f7d3d416
Saran440 <saranl@ecosoft.co.th>
2026-07-20 14:31:34 +0000

    [BOT] post-merge updates

--- budget_control_stock/__manifest__.py
+++ budget_control_stock/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Budget Control on Stock",
-    "version": "18.0.1.0.1",
+    "version": "18.0.2.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/ecosoft-odoo/budgeting",
